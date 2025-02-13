f5b4df169245c91fcf237efd9ed5fb6b3ff43e2d
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2025-02-13 11:41:04 +0100

    regisztracio

--- booksfrontend-main/books/src/app/components/regisztracio/regisztracio.component.ts
+++ booksfrontend-main/books/src/app/components/regisztracio/regisztracio.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-regisztracio',
-  standalone: false,
   templateUrl: './regisztracio.component.html',
-  styleUrl: './regisztracio.component.css'
+  styleUrls: ['./regisztracio.component.css'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class RegisztracioComponent {
+  registerForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const { name, email, password } = this.registerForm.value;
+      this.authService.register(name, email, password).subscribe({
+        next: (response) => {
+          if (response.status === 202) {
+            this.router.navigate(['/login']);
+            console.log(response);
+            
+          }
+        },
+        error: (error) => {
+          console.log(error);
+          this.errorMessage = error.error.message || 'Registration failed';
+        }
+      });
+    }
+  }
 }
